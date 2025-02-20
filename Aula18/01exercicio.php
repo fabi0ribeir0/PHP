b4a2909943c2e9a12a74cbe60e6af3c8942dae0e
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2025-02-20 11:20:44 -0300

    Criado base 18

---
+++ Aula18/01exercicio.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+<div>
+    <?php
+        
+    ?>    
+    <!-- <p><a href="exercicio01.html">Voltar</a></p> -->
+</div>    
+</body>
+</html>
