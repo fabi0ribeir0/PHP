9ec6698b728fdef177aafdb5e013bbd6aacb793c
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2025-02-21 16:23:07 -0300

    concluído aula 17

--- Aula17/01exercicio.php
+++ Aula17/01exercicio.php
@@ -9,9 +9,39 @@
 <body>
 <div>
     <?php
-        
+        $nome = "Fabio Ribeiro";
+        $minNome = "fabio ribeiro de lima";
+        $frase = "Estou na dedima setima aula";
+        $pos = strpos($frase, "setima");
+        $sub = substr($minNome, 0 ,3);
+        $pad = str_pad($nome, 30, "#", STR_PAD_RIGHT);
+        $replace = str_replace("bio", "ca" , $nome);
+        print("Seu nome é ". strtolower($nome));
+        echo "<p>". ucwords($minNome);
+        echo "<p>". strtolower(strrev($nome));
+        echo "<p> $pos";
+        echo "<p> $sub";
+        echo "<p>";
+        print(" EU $pad AQUI");
+        echo "<p> $replace";
+
+
     ?>    
-    <!-- <p><a href="exercicio01.html">Voltar</a></p> -->
+    <!-- 
+        strtolower - muda os caracteres para minusculas
+        strtoupper - muda os caracteres para maiusculas
+        ucfirst - Muda a primeira letra para maiuscula
+        ucwords - altera a primeira letra de cada palavra para maiusculas
+        strrev - inverte a ordem das letras
+        strpos - Mostra a posição da palavra escolhida no parametro
+        stripos - Faz o mesmo mas ignora maiusculas e minusculas
+        substr_count - conta quantas vezes a palavra escolhida no parametro é encontrada na string
+        substr - mostra x(3 no exemplo) letras apartir de y indice (0 no exemplo) cada letra é 1 indice
+        str_pad - Adiciona caracteres a esquerda direita ou centro para a string caber na quantidade citada
+        str_repeat - Repete x veses a string 
+        str_replace - subistitui uma palavra por outra (veja o exemplo) - pode ser uma parte da frase
+        str_ireplace - mema coisa ignorando maiusculas e minusculas
+    -->
 </div>    
 </body>
 </html>
