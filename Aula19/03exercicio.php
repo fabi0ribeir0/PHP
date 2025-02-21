82f9990f965f802ed37c0cb8bca25851a1ac252e
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2025-02-21 08:56:41 -0300

    criado base 19

---
+++ Aula19/03exercicio.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+</head>
+<body>
+<div>
+    <?php
+        
+    ?>
+    <p><a class="cm" href="javascript:history.go(-1)">Voltar</a> </p>
+    
+</div>    
+</body>
+</html>
