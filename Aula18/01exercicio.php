b5c133d9a99b219370ca14e32f4384de237350d6
Fábio Ribeiro <112023955+fabi0ribeir0@users.noreply.github.com>
2025-02-25 16:24:44 -0300

    Array PHP

--- Aula18/01exercicio.php
+++ Aula18/01exercicio.php
@@ -8,9 +8,17 @@
 </head>
 <body>
 <div>
-    <?php
-        
-    ?>    
+    <pre>
+        <?php
+            $n = array (3,5,8,2);
+            $n[] = 7;
+            print_r($n);
+
+            $c = range(5,20,5); // Indice 0 recebe 5 - até 20 - passo 5
+
+            print_r($c); // mostra 0=>5  1=>10   2=>15  3=>20
+        ?>
+    </pre>   
     <!-- <p><a href="exercicio01.html">Voltar</a></p> -->
 </div>    
 </body>
--- Aula18/02exercicio.php
+++ Aula18/02exercicio.php
@@ -8,10 +8,21 @@
 </head>
 <body>
 <div>
-    <?php
-        
-    ?> 
     
+    <pre>
+        <?php
+            $n = range(10,50,5);
+
+            foreach($n as $v){
+                echo "<p>[ $v ]";
+            }
+
+            $v = array ( 1=>5,  5=>9,  7=>8,  23=>7);
+            unset ($v[23]);
+            echo "<p>";
+            print_r($v);
+        ?> 
+    </pre>
       
     <p><a href="javascript:history.go(-1)">Voltar</a></p>
 </div>    
